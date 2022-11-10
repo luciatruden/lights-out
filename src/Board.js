96b8ca9e7716adb8471948a8af0c62f8c3595ce0
Lucia Trujillo <110251375+luciatruden@users.noreply.github.com>
2022-11-10 11:03:54 +0100

    create a size*size array matrix

--- src/Board.js
+++ src/Board.js
@@ -6,9 +6,22 @@ import Cell from "./Cell";
 function Board(props) {
     const { size } = props;
 
+    //create size*size matrix state, randomly setting isOn to true/false
+    const board = Array.from({ length: size }).map( 
+                 //row
+                 row => ( row = 
+                    Array.from({ length: size }).map(
+                        cell => (cell = true)
+                    )
+                 )
+
+    )
+
+    console.log(board)
+
     return(
         <div className="Board">
-            <Cell cellIndex="00" isOn={true} />
+            <Cell cellIndex="00" isOn={true}  />
             <Cell cellIndex="01" isOn={true} />
             <Cell cellIndex="02" isOn={false} />
             <Cell cellIndex="03" isOn={true} />
