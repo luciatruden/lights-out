74228bbff3cdcc4fa22eed5ef45d5be2807149f7
Lucia Trujillo <110251375+luciatruden@users.noreply.github.com>
2022-11-10 10:53:15 +0100

    create empty js and css files for Board component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
   return (
     <div className="App">
       <Cell cellIndez="00" isOn={true} />
+      <Cell cellIndez="01" isOn={true} />
+      <Cell cellIndez="02" isOn={true} />
+      <Cell cellIndez="03" isOn={true} />
+      <Cell cellIndez="04" isOn={true} />
+
     </div>
   );
 }
